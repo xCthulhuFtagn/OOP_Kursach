00cb49f7c213dd78741097a6c6cc84dfe3b6e5c0
Максим Колодяжный <mkolodyazhny@gmail.com>
2021-06-09 13:31:31 +0300

    main перенес в конец

--- OOP_Kursach/OOP_Kursach.cpp
+++ OOP_Kursach/OOP_Kursach.cpp
@@ -41,10 +41,46 @@ private:
     double AngVel, AngMom;
 };
 
-void Connect(GearWheel&, GearWheel&);
-double InnerConnect(GearWheel&, GearWheel&);
-double RatioAngMom(GearWheel& Spinner, GearWheel& Spinned);
-double RatioKinEn(GearWheel& Spinner, GearWheel& Spinned);
+void Connect(GearWheel& Spinner, GearWheel& Spinned) {
+    // i - передаточное число
+    double i = -(double)Spinned.GetRadius()/Spinner.GetRadius(); //при внешнем зацеплении направление вращения изменяется
+    Spinned.PutAngMom(Spinner.GetAngMom() * pow(i, 6));
+    Spinned.PutAngVel(Spinner.GetAngVel() * i);
+}
+
+double InnerConnect(GearWheel& Spinner, GearWheel& Spinned) {
+    double i = (double)Spinned.GetRadius() / Spinner.GetRadius();//при внутреннем зацеплении направление вращения не изменяется
+    Spinned.PutAngMom(Spinner.GetAngMom() * pow(i, 6));
+    Spinned.PutAngVel(Spinner.GetAngVel() * i);
+}
+
+double RatioAngMom(GearWheel& Spinner, GearWheel& Spinned) {
+   return pow(Spinner.GetRadius() / Spinned.GetRadius(), 4);
+}
+
+double RatioKinEn(GearWheel& Spinner, GearWheel& Spinned) {
+    return pow(Spinner.GetRadius() / Spinned.GetRadius(), 6);
+}
+
+GearWheel::GearWheel(unsigned TeethNumInp, double ToothLengthInp, double AngVelInp, double AngMomInp) 
+{
+    TeethNum = TeethNumInp;
+    ToothLength = ToothLengthInp;
+    AngVel = AngVelInp;
+    AngMom = AngMomInp;
+}
+unsigned GearWheel::GetTeethNum() { return TeethNum; }
+double GearWheel::GetRadius() { return (TeethNum+2)*ToothLength/4.5; /* R = D_e/2 = (z+2)*m/2 = (z+2)*h/4.5 */ }
+double GearWheel::GetWheelRimLength() { return (GetRadius() - GetToothLength()) * 2 * M_PI; }
+double GearWheel::GetRimDotsSpeed() { return AngVel * GetRadius(); }
+double GearWheel::GetKineticEnergy() { return AngMom * pow(AngVel, 2) / 2; }
+double GearWheel::GetToothLength() { return ToothLength; }
+double GearWheel::GetAngVel() { return AngVel; }
+double GearWheel::GetAngMom() { return AngMom; }
+void GearWheel::PutTeethNum(unsigned input) { TeethNum = input; }
+void GearWheel::PutToothLength(double input) { ToothLength = input; }
+void GearWheel::PutAngVel(double input) { AngVel = input; }
+void GearWheel::PutAngMom(double input) { AngMom = input; }
 
 int main()
 {
@@ -53,10 +89,10 @@ int main()
     setlocale(LC_ALL, "Russian");
     GearWheel G1(1.6, 0.25, 25.5, 16.7), G2, G3, G4;
     double i;
-    G2.PutTeethNum(10); 
+    G2.PutTeethNum(10);
     G2.PutToothLength(G1.GetToothLength());
     G3.PutTeethNum(12);
-    G3.PutToothLength(G2.GetToothLength()); 
+    G3.PutToothLength(G2.GetToothLength());
     G4.PutTeethNum(24);
     G4.PutToothLength(G3.GetToothLength());
     cout << "Входные данные" << endl;
@@ -97,46 +133,3 @@ int main()
     output << "Третье колесо: " << G3.GetRimDotsSpeed() << endl;
     output << "Четвертое колесо: " << G4.GetRimDotsSpeed() << endl;
 }
-
-void Connect(GearWheel& Spinner, GearWheel& Spinned) {
-    // i - передаточное число
-    double i = -(double)Spinned.GetRadius()/Spinner.GetRadius(); //при внешнем зацеплении направление вращения изменяется
-    Spinned.PutAngMom(Spinner.GetAngMom() * pow(i, 6));
-    Spinned.PutAngVel(Spinner.GetAngVel() * i);
-}
-
-double InnerConnect(GearWheel& Spinner, GearWheel& Spinned) {
-    double i = (double)Spinned.GetRadius() / Spinner.GetRadius();//при внутреннем зацеплении направление вращения не изменяется
-    Spinned.PutAngMom(Spinner.GetAngMom() * pow(i, 6));
-    Spinned.PutAngVel(Spinner.GetAngVel() * i);
-}
-
-double RatioAngMom(GearWheel& Spinner, GearWheel& Spinned) {
-   return pow(Spinner.GetRadius() / Spinned.GetRadius(), 4);
-}
-
-double RatioKinEn(GearWheel& Spinner, GearWheel& Spinned) {
-    return pow(Spinner.GetRadius() / Spinned.GetRadius(), 6);
-}
-
-//m = ToothLength / TeethNum
-GearWheel::GearWheel(unsigned TeethNumInp, double ToothLengthInp, double AngVelInp, double AngMomInp) 
-{
-    TeethNum = TeethNumInp;
-    ToothLength = ToothLengthInp;
-    AngVel = AngVelInp;
-    AngMom = AngMomInp;
-}
-unsigned GearWheel::GetTeethNum() { return TeethNum; }
-double GearWheel::GetRadius() { return (TeethNum+2)*ToothLength/4.5; /* R = D_e/2 = (z+2)*m/2 = (z+2)*h/4.5 */ }
-double GearWheel::GetWheelRimLength() { return (GetRadius() - GetToothLength()) * 2 * M_PI; }
-double GearWheel::GetRimDotsSpeed() { return AngVel * GetRadius(); }
-double GearWheel::GetKineticEnergy() { return AngMom * pow(AngVel, 2) / 2; }
-double GearWheel::GetToothLength() { return ToothLength; }
-double GearWheel::GetAngVel() { return AngVel; }
-double GearWheel::GetAngMom() { return AngMom; }
-void GearWheel::PutTeethNum(unsigned input) { TeethNum = input; }
-void GearWheel::PutToothLength(double input) { ToothLength = input; }
-void GearWheel::PutAngVel(double input) { AngVel = input; }
-void GearWheel::PutAngMom(double input) { AngMom = input; }
-
